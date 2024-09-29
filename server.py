68f5e7e39dedc9ed5a8ff3f2f75254e5306a58b4
Karthik Krapa <karthik.krapa18@Karthiks-MacBook-Air.local>
2024-09-29 15:43:41 -0400

    Changes to server.py for autograder

--- server.py
+++ server.py
@@ -1,53 +1,39 @@
 from socket import *
-import sys # In order to terminate the program
+import sys  # In order to terminate the program
 
 def webServer(port):
     serverPort = port 
-    serverSocket = socket(AF_INET, SOCK_STREAM)#server socket - TCP byte stream
-    serverSocket.bind(('',serverPort)) #binding port to server port
-    serverSocket.listen(1) #server begins listening for incoming requests
+    serverSocket = socket(AF_INET, SOCK_STREAM)  # server socket - TCP byte stream
+    serverSocket.bind(('', serverPort))  # binding port to server port
+    serverSocket.listen(1)  # server begins listening for incoming requests
     print('Server being set up...')
 
     while True:
-        print ('Ready to serve...')
-        connectionSocket, addr = serverSocket.accept() #server waits on accept new socket returned on request as this is TCP
+        print('Ready to serve...')
+        connectionSocket, addr = serverSocket.accept()  # server waits on accept new socket returned on request as this is TCP
         print('Request accepted from:', addr)
         try:
-            message = connectionSocket.recv(1024).decode() #decodes the received byte stream message
+            message = connectionSocket.recv(1024).decode()  # decodes the received byte stream message
             filename = message.split()[1]
-            f = open(filename[1:],'r')
-            outputdata = f.read()
+            with open(filename[1:], 'r') as f:  # Use 'with' to automatically close the file
+                outputdata = f.read()
 
-            headerLine = 'HTTP/1.1 200 OK\r\n' #header line containing response status code and status phrase
-            connectionSocket.send(headerLine.encode()) #encoding to send back
+            headerLine = 'HTTP/1.1 200 OK\r\n'
+            connectionSocket.send(headerLine.encode())  # encoding to send back
             connectionSocket.send("Content-Type: text/html\r\n".encode())
-            connectionSocket.send('\r\n'.encode()) #carriage return otherwise the headers won't be displayed
+            connectionSocket.send('\r\n'.encode())  # end of headers
 
-            for i in range(0, len(outputdata)):
-                connectionSocket.send(outputdata[i].encode())
-            connectionSocket.send('\r\n'.encode())
-
+            connectionSocket.send(outputdata.encode())  # Send entire output data at once
             connectionSocket.close()
         
         except IOError:
-            errHeaderLine = 'HTTP/1.1 404 Not Found\r\n'  #r for carriage return and n for new line
+            errHeaderLine = 'HTTP/1.1 404 Not Found\r\n'
             connectionSocket.send(errHeaderLine.encode())
             connectionSocket.send('\r\n'.encode())
-            f2 = open('404page.html','r')
-            outputdata_err = f2.read()
-
-            for i in range(0, len(outputdata_err)):
-                connectionSocket.send(outputdata_err[i].encode())
-            connectionSocket.send('\r\n'.encode())
+            with open('404page.html', 'r') as f2:  # Use 'with' for error page as well
+                outputdata_err = f2.read()
+                connectionSocket.send(outputdata_err.encode())  # Send entire error page at once
             connectionSocket.close()
-        
-        serverSocket.close()
-        sys.exit()
-
 
 if __name__ == '__main__':
     webServer(6879)
-
-
-
-
