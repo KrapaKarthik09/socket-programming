c476d8533a65d436c21b2f907e15613cc07c3eb7
Karthik Krapa <karthik.krapa18@10-18-217-84.dynapool.wireless.nyu.edu>
2024-10-13 13:42:53 -0400

    Fix Autograder to client.py file

--- client.py
+++ client.py
@@ -1,39 +1,32 @@
 # Import socket module
 # Import time and ctime to retrieve time
-# Import sys to retrieve the arguments
 from socket import *
 from time import time, ctime
 import sys
 
-def main(serverHost, serverPort):
-    # Preparing the socket
-    clientSocket = socket(AF_INET, SOCK_DGRAM)
-    clientSocket.settimeout(1)
+# Define server host and port
+serverHost = 'localhost'  # or '127.0.0.1'
+serverPort = 12000  # Specify your server port here
 
-    for i in range(10):
-        startTime = time()  # Retrieve the current time
-        message = f"Ping {i + 1} {ctime(startTime)[11:19]}"
+# Preparing the socket
+clientSocket = socket(AF_INET, SOCK_DGRAM)
+clientSocket.settimeout(1)
 
-        try:
-            # Sending the message and waiting for the answer
-            clientSocket.sendto(message.encode(), (serverHost, int(serverPort)))
-            encodedModified, serverAddress = clientSocket.recvfrom(1024)
+for i in range(10):
+    startTime = time()  # Retrieve the current time
+    message = f"Ping {i+1} {startTime:.6f}"  # Include time in seconds
 
-            # Checking the current time and if the server answered
-            endTime = time()
-            modifiedMessage = encodedModified.decode()
-            print(modifiedMessage)
-            print("RTT: %.3f ms\n" % ((endTime - startTime) * 1000))
-        except timeout:
-            print(f"PING {i + 1} Request timed out\n")
+    try:
+        # Sending the message and waiting for the answer
+        clientSocket.sendto(message.encode(), (serverHost, serverPort))
+        encodedModified, serverAddress = clientSocket.recvfrom(1024)
 
-    clientSocket.close()
+        # Checking the current time and if the server answered
+        endTime = time()
+        modifiedMessage = encodedModified.decode()
+        print(modifiedMessage)
+        print("RTT: %.3f ms\n" % ((endTime - startTime) * 1000))
+    except timeout:
+        print("PING %i Request timed out\n" % (i + 1))
 
-if __name__ == '__main__':
-    # Checking to see if we have three arguments
-    if len(sys.argv) != 3:
-        print("Wrong number of arguments.")
-        print("Use: client.py <server_host> <server_port>")
-    else:
-        serverHost, serverPort = sys.argv[1:]
-        main(serverHost, serverPort)
+clientSocket.close()
