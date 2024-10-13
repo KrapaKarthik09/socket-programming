236a6426234450f5d5b97a52f01f80c29af97595
Karthik Krapa <karthik.krapa18@10-18-217-84.dynapool.wireless.nyu.edu>
2024-10-13 13:52:24 -0400

    Fix: client.py update for Autograder

--- client.py
+++ client.py
@@ -1,32 +1,44 @@
 # Import socket module
-# Import time and ctime to retrieve time
 from socket import *
 from time import time
 import sys
 
-# Define server host and port
-serverHost = 'localhost'  # or '127.0.0.1'
-serverPort = 12000  # Specify your server port here
+def ping(serverHost, serverPort):
+    # Preparing the socket
+    clientSocket = socket(AF_INET, SOCK_DGRAM)
+    clientSocket.settimeout(1)
+    
+    # List to store results
+    results = []
 
-# Preparing the socket
-clientSocket = socket(AF_INET, SOCK_DGRAM)
-clientSocket.settimeout(1)
+    for i in range(10):
+        startTime = time()  # Retrieve the current time
+        message = f"Ping {i + 1} {startTime:.6f}"  # Include time in seconds
 
-for i in range(10):
-    startTime = time()  # Retrieve the current time
-    message = f"Ping {i + 1} {startTime:.6f}"  # Include time in seconds
+        try:
+            # Sending the message and waiting for the answer
+            clientSocket.sendto(message.encode(), (serverHost, serverPort))
+            encodedModified, serverAddress = clientSocket.recvfrom(1024)
 
-    try:
-        # Sending the message and waiting for the answer
-        clientSocket.sendto(message.encode(), (serverHost, serverPort))
-        encodedModified, serverAddress = clientSocket.recvfrom(1024)
+            # Checking the current time and if the server answered
+            endTime = time()
+            modifiedMessage = encodedModified.decode()
+            rtt = (endTime - startTime) * 1000  # RTT in milliseconds
+            
+            # Append results as a tuple (sequence number, server response, RTT)
+            results.append((i + 1, modifiedMessage.strip(), rtt))
+        except timeout:
+            print(f"PING {i + 1} Request timed out\n")
 
-        # Checking the current time and if the server answered
-        endTime = time()
-        modifiedMessage = encodedModified.decode()
-        print(modifiedMessage)
-        print("RTT: %.3f ms\n" % ((endTime - startTime) * 1000))
-    except timeout:
-        print("PING %i Request timed out\n" % (i + 1))
+    clientSocket.close()
+    return results
 
-clientSocket.close()
+if __name__ == "__main__":
+    # Define server host and port
+    if len(sys.argv) != 3:
+        print("Usage: python client.py <server_host> <server_port>")
+        sys.exit(1)
+
+    serverHost, serverPort = sys.argv[1], int(sys.argv[2])
+    response = ping(serverHost, serverPort)
+    print("Client Response:", response)
