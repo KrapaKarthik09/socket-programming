9cf5beca92fe38d7af71af18ea6ac23b6424f9bc
Karthik Krapa <54001172+KrapaKarthik09@users.noreply.github.com>
2024-10-13 14:27:05 -0400

    Update HeartbeatClient.py

--- HeartbeatClient.py
+++ HeartbeatClient.py
@@ -3,22 +3,22 @@ import time
 import sys
 
 def heartbeat(host, port, interval=2):
-    seq = 0  #Sequence number for heartbeat messages
+    seq = 0  # Sequence number for heartbeat messages
 
     clientSocket = socket(AF_INET, SOCK_DGRAM)
 
     while True:
         try:
-            current_time = time.time()  #Current time
+            current_time = time.time()  # Current time
             message = f"Heartbeat {seq} {current_time}"
 
-            #Sending heartbeat to server
+            # Sending heartbeat to server
             clientSocket.sendto(message.encode(), (host, port))
 
             print(f"Sent Heartbeat {seq} at {current_time}")
             seq += 1
 
-            #Wait for the specified interval before sending the next heartbeat
+            # Wait for the specified interval before sending the next heartbeat
             time.sleep(interval)
 
         except KeyboardInterrupt:
