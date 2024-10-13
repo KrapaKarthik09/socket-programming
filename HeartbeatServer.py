a10aee5ab0e25fc0a477cb8e92fc10a733d33407
Karthik Krapa <54001172+KrapaKarthik09@users.noreply.github.com>
2024-10-13 14:27:30 -0400

    Update HeartbeatServer.py

--- HeartbeatServer.py
+++ HeartbeatServer.py
@@ -5,8 +5,8 @@ import sys
 def serve(port, heartbeat_timeout=5):
     serverSocket = socket(AF_INET, SOCK_DGRAM)
     serverSocket.bind(('', port))
-    last_seq = -1  #to keep track of sequence number for detecting lost packets
-    last_heartbeat_time = time.time()  #the last heartbeat was received time
+    last_seq = -1  # to keep track of sequence number for detecting lost packets
+    last_heartbeat_time = time.time()  # the last heartbeat was received time
 
     print("Server is ready to receive heartbeat signals.")
     
@@ -14,19 +14,19 @@ def serve(port, heartbeat_timeout=5):
         try:
             message, address = serverSocket.recvfrom(1024)
             current_time = time.time()
-            heartbeat = message.decode().split() #heartbeat message decode
+            heartbeat = message.decode().split()  # heartbeat message decode
             seq = int(heartbeat[1])
             c_time = float(heartbeat[2])
 
             print(f"Received Heartbeat {seq} from {address} at {current_time}, sent at {c_time}")
 
-            if seq != last_seq + 1 and last_seq != -1: #checking pakect loss
+            if seq != last_seq + 1 and last_seq != -1:  # checking packet loss
                 print(f"Warning: Packet loss detected! Expected seq {last_seq + 1}, but got {seq}.")
 
-            if current_time - last_heartbeat_time > heartbeat_timeout: #checking heartbeat timeout
+            if current_time - last_heartbeat_time > heartbeat_timeout:  # checking heartbeat timeout
                 print("Warning: Heartbeat timeout. Client may have stopped.")
             
-            #Updating last sequence number and heartbeat time
+            # Updating last sequence number and heartbeat time
             last_seq = seq
             last_heartbeat_time = current_time
 
